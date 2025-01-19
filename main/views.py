6711f75d0a9dbfed1366823bf7d029c89220176f
DarrenM07 <darrenms7120@gmail.com>
2025-01-20 00:36:33 +0700

    about us, home

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2306256293',
+        'name': 'Darren',
+        'class': 'OKE OCE'
+    }
+
+    return render(request, "main.html", context)
